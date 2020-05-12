78b0cbb00ecdc789f575b7835eff10715293d7cc
jesus-cm1907 <56446966+jesus-cm1907@users.noreply.github.com>
2020-05-13 01:57:51 +0200

    Add files via upload

---
+++ Entornos/src/PaginaWeb.java
@@ -0,0 +1,24 @@
+
+public class PaginaWeb {
+public String Inicio;
+public String Menu_Principal;
+public String clasificacion;
+public String Buscador;
+
+public PaginaWeb() {}
+
+
+public  PaginaWeb(String Inicio,String Menu_Principal,String clasificacion,String Buscador) {
+	
+	this.Inicio=Inicio;
+	this.Menu_Principal=Menu_Principal;
+	this.clasificacion=clasificacion;
+	this.Buscador=Buscador;
+	
+}
+
+
+
+ public void desplazarse_en_la_pagina() {}
+
+}
