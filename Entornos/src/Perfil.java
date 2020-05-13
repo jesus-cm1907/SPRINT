b5847c644af0baf03bd6014da0b3d7be3ff777e6
jesus-cm1907 <56446966+jesus-cm1907@users.noreply.github.com>
2020-05-13 22:06:23 +0200

    Sprint3 (#7)
    * Delete Diagrama en blanco.png
    
    * Delete Sprint1.png
    
    * Delete README.md
    
    * Delete Welcome file (1).md
    
    * Add files via upload
    
    * Add files via upload

---
+++ Entornos/src/Perfil.java
@@ -0,0 +1,18 @@
+
+public class Perfil {
+public String nombre;
+public String correo;
+public int edad;
+
+public Perfil() {}
+
+public Perfil(String nombre,String correo,int edad) {
+	
+	this.nombre=nombre;
+	this.correo=correo;
+	this.edad=edad;
+}
+
+public void navegarperfil() {}
+
+}
---
+++ Entornos/src/Registrarse.java
@@ -0,0 +1,22 @@
+
+public class Registrarse {
+public String nombre_usuario;
+public String correo;
+public String contraseña;
+public int fecha_nacimiento;
+
+public Registrarse() {}
+
+public Registrarse(String nombre_usuario,String correo,String contraseña,int fecha_nacimiento ) {
+	this.nombre_usuario=nombre_usuario;
+	this.correo=correo;
+	this.contraseña=contraseña;
+	this.fecha_nacimiento=fecha_nacimiento;
+	
+}
+
+
+
+public void crearusuario(String nombre_usuario,String correo,String contraseña,int fecha_nacimiento) {}
+
+}
